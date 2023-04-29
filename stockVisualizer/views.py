a2510fbb32168253e55cab8dea66192f3bdca98e
Andrew Maher <amaher423@gmail.com>
2023-04-28 23:09:08 -0500

    Added frontend and backend for visualizing stock simple moving average

---
+++ stockVisualizer/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import StockData
+from decouple import config
+
+import requests
+import json
+
+API_KEY = config("ALPHAVANTAGE_API_KEY")
+
+DATABASE_ACCESS = True
+
+def home(request):
+    return render(request, 'home.html', {})
+
+
+@csrf_exempt
+def get_stock_data(request):
+    if request.is_ajax():
+        #get ticker from the AJAX POST request
+        ticker = request.POST.get('ticker', 'null')
+        ticker = ticker.upper()
+
+        if DATABASE_ACCESS == True:
+            #checking if the database already has data stored for this ticker before querying the Alpha Vantage API
+            if StockData.objects.filter(symbol=ticker).exists(): 
+                #We have the data in our database! Get the data from the database directly and send it back to the frontend AJAX call
+                entry = StockData.objects.filter(symbol=ticker)[0]
+                return HttpResponse(entry.data, content_type='application/json')
+
+        #obtain stock data from Alpha Vantage APIs
+        #get adjusted close data
+        price_series = requests.get(f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={ticker}&apikey={API_KEY}&outputsize=full').json()
+        
+        #get SMA (simple moving average) data
+        sma_series = requests.get(f'https://www.alphavantage.co/query?function=SMA&symbol={ticker}&interval=daily&time_period=10&series_type=close&apikey={API_KEY}').json()
+
+        #package up the data in an output dictionary 
+        output_dictionary = {}
+        output_dictionary['prices'] = price_series
+        output_dictionary['sma'] = sma_series
+
+        #save the dictionary to database
+        temp = StockData(symbol=ticker, data=json.dumps(output_dictionary))
+        temp.save()
+
+        #return the data back to the frontend AJAX call 
+        return HttpResponse(json.dumps(output_dictionary), content_type='application/json')
+
+    else:
+        message = "Not Ajax"
+        return HttpResponse(message)
